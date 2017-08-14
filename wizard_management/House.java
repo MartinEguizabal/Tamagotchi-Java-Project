1d458306d8567339140b7d3a5a07a034adbc1a19
MartyCodeClan <zabbalab@hotmail.com>
2017-08-14 08:05:52 +0100

    added basic methods getters and setters to House and Campus classes

--- wizard_management/House.java
+++ wizard_management/House.java
@@ -11,6 +11,48 @@ public class House{
     this.name = name;
     this.contract = contract;
     this.drama_factor = drama_factor;
-    Tamagotchi[] members;
+    this.members = new Tamagotchi[10];
   }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public Boolean getContract(){
+    return this.contract;
+  }
+
+  public void setContract(Boolean contract){
+    this.contract = contract;
+  }
+
+  public int getDramaFactor(){
+    return this.drama_factor;
+  }
+
+  public void setDramaFactor(int drama_factor){
+    this.drama_factor = drama_factor;
+    if (this.drama_factor < 0){
+      this.drama_factor = 0;
+    }
+    if(this.drama_factor > 10){
+      this.drama_factor = 10;
+    }
+  }
+
+  public int countMembers(){
+    int count = 0;
+    for(Tamagotchi member : this.members){
+      if(member != null){
+        count ++;
+      } 
+    }
+    return count;
+  }
+
+
 }
--- wizard_management_specs/HouseTest.java
+++ wizard_management_specs/HouseTest.java
@@ -1,19 +1,64 @@
-// import wizard_management.*;
-// import static org.junit.Assert.*;
-// import org.junit.*;
-// import java.util.*;
+import wizard_management.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
 
-// public class HouseTest{
+public class HouseTest{
 
-//   House house1;
-//   House house2;
-//   House house3;
+  House house1;
+  House house2;
+  House house3;
+  WickedlyTamagotchi wickedlytamagotchi1;
+  WickedlyTamagotchi wickedlytamagotchi2;
+  WickedlyTamagotchi wickedlytamagotchi3;
 
-//   @Before
-//   public void before(){
-//     house1 = new House("Veritable", false, 0);
-//     house2 = new House("The Honky Tonk", false, 0);
-//     house3 = new House("Tomogatchi Groto", false, 0);
-//   }
+  @Before
+  public void before(){
+    house1 = new House("Veritable", false, 0);
+    house2 = new House("The Honky Tonk", false, 0);
+    house3 = new House("Tomogatchi Groto", false, 0);
+    wickedlytamagotchi1 = new WickedlyTamagotchi("Wyvern", 0, 0, 0, 0, false, 1);
+    wickedlytamagotchi2 = new WickedlyTamagotchi("Testy", 8, 4, 4, 4, true, 80);
+    wickedlytamagotchi3 = new WickedlyTamagotchi("Excess", 10, 0, 10, 1, true, 90);
+  }
 
-// }
+  @Test
+  public void canGetName(){
+    assertEquals("Veritable", house1.getName());
+  }
+
+  @Test
+  public void canSetName(){
+    house1.setName("Big House");
+    assertEquals("Big House", house1.getName());
+  }
+
+  @Test
+  public void canGetContract(){
+    assertEquals(false, house1.getContract());
+  }
+
+  @Test
+  public void canSetContract(){
+    house1.setContract(true);
+    assertEquals(true, house1.getContract());
+  }
+
+  @Test
+  public void canGetDramaFactor(){
+    assertEquals(0, house1.getDramaFactor());
+  }
+
+  @Test
+  public void canSetDramaFactor(){
+    house1.setDramaFactor(5);
+    assertEquals(5, house1.getDramaFactor());
+  }
+
+  // @Test
+  // public void canCountMembers(){
+    
+  // }
+
+
+}
