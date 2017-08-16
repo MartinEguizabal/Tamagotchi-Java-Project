f94ab63d03013d63bd76f1437d738c16e1599f30
MartyCodeClan <zabbalab@hotmail.com>
2017-08-16 11:01:38 +0100

    added basic add remove and find methods in campus class

--- wizard_management/Campus.java
+++ wizard_management/Campus.java
@@ -8,7 +8,36 @@ public class Campus{
 
   public Campus(String name){
     this.name = name;
-    ArrayList<House> houses;
+    this.houses = new ArrayList<House>();
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public int countHouses(){
+    return this.houses.size();
+  }
+
+  public void addHouse(House house){
+    this.houses.add(house);
+  }
+
+  public void removeHouse(House house){
+    this.houses.remove(house);
+  }
+
+  public String findHouse(House house){
+    for(House the_house : this.houses){
+      if(the_house == house){
+        return the_house.getName();
+      }
+    }
+    return "Can't find house";
   }
 
 
--- wizard_management_specs/CampusTest.java
+++ wizard_management_specs/CampusTest.java
@@ -1,14 +1,49 @@
-// import wizard_management.*;
-// import static org.junit.Assert.*;
-// import org.junit.*;
-// import java.util.*;
+import wizard_management.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
 
-// public class CampusTest{
+public class CampusTest{
 
-//   Campus campus1;
+  Campus campus1;
+  House house1;
+  House house2;
+  House house3;
 
-//   @Before
-//   public void before(){
-//     campus1 = new Campus("Tamagotchi College Campus");
-//   }
-// }
+  @Before
+  public void before(){
+    campus1 = new Campus("Tamagotchi College Campus");
+    house1 = new House("Veritable", false, 0);
+    house2 = new House("The Honky Tonk", false, 0);
+    house3 = new House("Tomogatchi Groto", false, 0);
+  }
+
+  @Test
+  public void canAddHouse(){
+    campus1.addHouse(house3);
+    assertEquals(1, campus1.countHouses());
+    assertEquals("Tomogatchi Groto", campus1.findHouse(house3));
+  }
+
+  @Test
+  public void canRemoveHouse(){
+    campus1.addHouse(house1);
+    campus1.addHouse(house3);
+    campus1.removeHouse(house1);
+    assertEquals("Can't find house", campus1.findHouse(house1));
+  }
+
+  @Test
+  public void canFindHouse(){
+    campus1.addHouse(house1);
+    campus1.addHouse(house2);
+    assertEquals("The Honky Tonk", campus1.findHouse(house2));
+  }
+
+  @Test
+  public void cannotFindHouse(){
+    campus1.addHouse(house1);
+    campus1.addHouse(house2);
+    assertEquals("Can't find house", campus1.findHouse(house3));
+  }
+}
